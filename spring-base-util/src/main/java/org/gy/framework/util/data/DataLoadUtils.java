9b46879f4ef289f8f72f82c18789346728e321ff
guanyang <guanyangsunlight@163.com>
2022-03-16 20:44:38 +0800

    update util support

--- spring-base-util/src/main/java/org/gy/framework/util/data/DataLoadUtils.java
+++ spring-base-util/src/main/java/org/gy/framework/util/data/DataLoadUtils.java
@@ -1,12 +1,15 @@
 package org.gy.framework.util.data;
 
+import com.google.common.collect.Lists;
 import java.util.Collection;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 功能描述：数据加载工具类
@@ -14,7 +17,35 @@ import lombok.NoArgsConstructor;
  * @author gy
  * @version 1.0.0
  */
+@Slf4j
 public class DataLoadUtils {
+
+
+    /**
+     * 功能描述：批量获取，避免超过单次查询限制
+     *
+     * @param params 方法入参
+     * @param executeFun 执行函数
+     * @param batchSize 批量大小
+     * @author gy
+     * @version 1.0.0
+     */
+    public static <T, R> List<R> batchExecute(List<T> params, Function<List<T>, List<R>> executeFun, int batchSize) {
+        List<R> result = Lists.newArrayList();
+        if (isEmpty(params)) {
+            log.warn("[batchExecute]批量获取参数为空:batchSize={}", batchSize);
+            return result;
+        }
+        //去重处理
+        List<T> reqList = params.stream().distinct().collect(Collectors.toList());
+        //分批获取，避免超过批量限制
+        List<List<T>> partition = Lists.partition(reqList, batchSize);
+        partition.forEach(items -> {
+            List<R> resList = executeFun.apply(items);
+            result.addAll(resList);
+        });
+        return result;
+    }
 
     /**
      * 功能描述：分批加载处理数据
