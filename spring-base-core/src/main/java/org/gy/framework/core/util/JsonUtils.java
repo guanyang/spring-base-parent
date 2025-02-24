813651f0ae456c5040826e45f483e7d13d486a5a
guanyang <guanyangsunlight@163.com>
2025-02-24 17:50:16 +0800

    update core

--- spring-base-core/src/main/java/org/gy/framework/core/util/JsonUtils.java
+++ spring-base-core/src/main/java/org/gy/framework/core/util/JsonUtils.java
@@ -2,8 +2,14 @@ package org.gy.framework.core.util;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.TypeAdapter;
+import com.google.gson.TypeAdapterFactory;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
 import java.io.Reader;
 import java.lang.reflect.Type;
 import java.util.function.BiFunction;
@@ -21,6 +27,7 @@ public class JsonUtils {
     private static final Gson GSON = new GsonBuilder()
             .disableInnerClassSerialization()           //禁此序列化内部类
             .disableHtmlEscaping()                      //禁止转义html标签
+            .registerTypeAdapterFactory(new LenientTypeAdapterFactory()) //跳过错误字段
             .create();
 
     private JsonUtils() {
@@ -58,6 +65,35 @@ public class JsonUtils {
         } catch (Throwable e) {
             log.warn("[JsonUtils]execute error: t={}, u={}", t, u, e);
             return null;
+        }
+    }
+
+    public static class LenientTypeAdapterFactory implements TypeAdapterFactory {
+        @Override
+        public <T> TypeAdapter<T> create(Gson gson, TypeToken<T> type) {
+            final TypeAdapter<T> delegate = gson.getDelegateAdapter(this, type);
+            return new TypeAdapter<T>() {
+                @Override
+                public void write(JsonWriter out, T value) throws IOException {
+                    try {
+                        delegate.write(out, value);
+                    } catch (Exception e) {
+                        // 序列化时忽略错误字段
+                        out.nullValue();
+                    }
+                }
+
+                @Override
+                public T read(JsonReader in) throws IOException {
+                    try {
+                        return delegate.read(in);
+                    } catch (Exception e) {
+                        // 反序列化时跳过错误字段
+                        in.skipValue();
+                        return null;
+                    }
+                }
+            };
         }
     }
 
