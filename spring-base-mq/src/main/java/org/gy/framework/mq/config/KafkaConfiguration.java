31ba99ad2ff42f14a194094357cf0a2fef413867
guanyang <guanyangsunlight@163.com>
2025-09-12 16:34:46 +0800

    update mq module

--- spring-base-mq/src/main/java/org/gy/framework/mq/config/KafkaConfiguration.java
+++ spring-base-mq/src/main/java/org/gy/framework/mq/config/KafkaConfiguration.java
@@ -2,13 +2,11 @@ package org.gy.framework.mq.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.gy.framework.mq.annotation.ConditionalOnNonEmptyCollection;
-import org.gy.framework.mq.config.MqProperties.GlobalConfig;
+import org.gy.framework.mq.config.support.KafkaDefaultErrorHandler;
 import org.gy.framework.mq.core.EventLogService;
 import org.gy.framework.mq.core.support.DefaultKafkaMessageHandler;
-import org.gy.framework.mq.core.support.EventMessageHandlerParseFactory;
 import org.gy.framework.mq.listener.DefaultKafkaListener;
-import org.gy.framework.mq.model.EventLogContext;
-import org.gy.framework.mq.model.EventMessageHandlerContext;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
@@ -16,13 +14,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.listener.DefaultErrorHandler;
-import org.springframework.kafka.support.ExponentialBackOffWithMaxRetries;
 import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
 
-import java.util.Collections;
-
 import static org.gy.framework.mq.config.MqProperties.KAFKA_PREFIX;
-import static org.gy.framework.mq.model.MqType.KAFKA;
 
 @Slf4j
 @Configuration(proxyBeanMethods = false)
@@ -44,22 +38,8 @@ public class KafkaConfiguration {
 
     @Bean
     @ConditionalOnMissingBean(DefaultErrorHandler.class)
-    public DefaultErrorHandler defaultErrorHandler(MqProperties properties, EventLogService eventLogService) {
-        GlobalConfig globalConfig = properties.getGlobalConfig();
-        ExponentialBackOffWithMaxRetries backOff = new ExponentialBackOffWithMaxRetries(globalConfig.getRetryTimes());
-        backOff.setInitialInterval(globalConfig.getInitialInterval());
-        backOff.setMultiplier(globalConfig.getMultiplier());
-        backOff.setMaxInterval(globalConfig.getMaxInterval());
-        DefaultErrorHandler errorHandler = new DefaultErrorHandler((record, ex) -> {
-            EventMessageHandlerContext context = EventMessageHandlerParseFactory.parse(KAFKA, record);
-            if (!EventMessageHandlerContext.validate(context)) {
-                log.warn("EventMessageHandlerContext validate fail: {}", record, ex);
-                return;
-            }
-            EventLogContext.handleEventLog(Collections.singletonList(context.getEventMessage()), ex, eventLogService::batchSaveEventLog);
-        }, backOff);
-        // 配置哪些异常不触发重试
-//        errorHandler.addNotRetryableExceptions(IllegalArgumentException.class);
+    public DefaultErrorHandler defaultErrorHandler(ObjectProvider<MqProperties> properties, ObjectProvider<EventLogService> eventLogService) {
+        DefaultErrorHandler errorHandler = new KafkaDefaultErrorHandler(properties, eventLogService);
         errorHandler.setCommitRecovered(true);
         return errorHandler;
     }
